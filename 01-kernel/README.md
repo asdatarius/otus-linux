8cbea51d1dadb9bbe298ae360e6edc32ebdc6e49
Evgeny Prokopyev <prokopiev@corp.mail.ru>
2019-11-20 23:46:19 -0800

    Reorganize storage for linux homeworks.

---
+++ 01-kernel/README.md
@@ -0,0 +1,8 @@
+# Create box from scratch for vagrant, with guest additions
+Use "stable" kernel.
+
+Latest vbox guest additions were used, keep it in mind (6.0.14). 
+If you want to use latest kernel, have to modify script 1 with hardcoded urls.
+
+# Instructions used
+Additional step by step guide can be found at [the manual](https://github.com/asdatarius/manual_kernel_update/manual/manual.rd).
---
+++ 01-kernel/packer/scripts/stage-1-kernel-update.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Install elrepo
+#yum install -y http://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
+## Install new kernel
+#yum --enablerepo elrepo-kernel install kernel-ml -y
+## Remove older kernels (Only for demo! Not Production!)
+#rm -f /boot/*3.10*
+
+# Install dependecies, download kernel sources, config/make/make install
+yum install -y ncurses-devel make gcc bc bison flex elfutils-libelf-devel openssl-devel grub2 wget perl bzip2 tar && cd /usr/src/ && wget https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-5.3.8.tar.xz && tar -xf linux-5.3.8.tar.xz && cd linux-5.3.8 && cp -v /boot/config-$(uname -r) .config && make olddefconfig && make -j4 && make modules_install && make install
+
+# Update GRUB
+grub2-mkconfig -o /boot/grub2/grub.cfg
+grub2-set-default 0
+echo "Grub update done."
+# Reboot VM
+shutdown -r now
