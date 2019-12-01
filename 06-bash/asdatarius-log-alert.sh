8b197d4e6cdacd68278de516047d3c48953d856f
Evgeny Prokopyev <prokopiev@corp.mail.ru>
2019-12-01 22:56:54 +0300

    Add log alert service, start on vagrant start

---
+++ 06-bash/asdatarius-log-alert.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+PATH_TO_LOGFILE=$1
+PATH_TO_LOCKFILE=/tmp/asdatarius-log-alert.lock
+PATH_TO_PROGRESS_STORAGE=/tmp/asdatarius-log-alert.progress
+ALERT_EMAIL="vagrant@localhost"
+
+# main usage through systemd service, so redundant
+if ( set -o noclobber; echo "$$" > "$PATH_TO_LOCKFILE") 2> /dev/null; then
+	trap 'rm -f "$PATH_TO_LOCKFILE"; exit $?' INT TERM EXIT
+	
+	if [[ ! -r $PATH_TO_LOGFILE ]]; then
+		echo "File \"${PATH_TO_LOGFILE}\" must be readable"
+		exit 1
+	fi
+	
+	SIZE=$(stat --printf="%s" "${PATH_TO_LOGFILE}")
+	PREV_SIZE=$(cat ${PATH_TO_PROGRESS_STORAGE})
+
+	if [[ $SIZE -lt $PREV_SIZE ]]; then
+		# log was rotated
+		SIZE=0
+		PREV_SIZE=0
+	elif [[ $SIZE -eq $PREV_SIZE ]]; then
+		# log still the same
+		echo "File \"${PATH_TO_LOGFILE}\" still the same, skip!"
+		exit 0
+	fi
+
+	echo ${SIZE} > ${PATH_TO_PROGRESS_STORAGE}
+
+	((PREV_SIZE++))
+	IPs=$(tail -c +${PREV_SIZE} ${PATH_TO_LOGFILE} | awk 'NF' | awk '{ print $1 }' | sort | uniq -c | sort -nr | head -n 10)
+	# there are broken uri (no method/uri/protocol)
+	URIs=$(tail -c +${PREV_SIZE} ${PATH_TO_LOGFILE} | awk 'NF' | awk -F'"' '{ print $2 }' | awk '{ if(length($2) != 0) { print $2 } else print "!!!BINGO BONGO!!!" }' | sort | uniq -c | sort -nr | head -n 10)
+	# broken request part leads to broken numbering, so go for quote delimiter first
+	ERR_CODEs=$(tail -c +${PREV_SIZE} ${PATH_TO_LOGFILE} | awk 'NF' | awk -F'"' '{ print $3 }' | awk '{print $1}' | grep -v '^[^45]' | sort | uniq -c | sort -nr)
+	OK_CODEs=$(tail -c +${PREV_SIZE} ${PATH_TO_LOGFILE} | awk 'NF' | awk -F'"' '{ print $3 }' | awk '{print $1}' | grep -v '^[45]' | sort | uniq -c | sort -nr)
+
+
+	/usr/sbin/sendmail -t 2> /dev/null <<LOGMAIL
+To: ${ALERT_EMAIL}
+From: systemd <root@${HOSTNAME}>
+Subject: asdatarius-log-alert.service
+Content-Transfer-Encoding: 8bit
+Content-Type: text/plain; charset=UTF-8
+
+LOG:
+${PATH_TO_LOGFILE}:${PREV_SIZE}-EOF
+
+IP TOP:
+${IPs}
+
+URI TOP:
+${URIs}
+
+ERRORS:
+${ERR_CODEs}
+
+REQUESTS:
+${OK_CODEs}
+
+Your monitoring.
+LOGMAIL
+
+ 	rm -f "${PATH_TO_LOCKFILE}"
+ 	trap - INT TERM EXIT
+else
+	echo "Failed to acquire lockfile: ${PATH_TO_LOCKFILE}."
+	echo "Held by $(cat ${PATH_TO_LOCKFILE})"
+	exit 1
+fi
